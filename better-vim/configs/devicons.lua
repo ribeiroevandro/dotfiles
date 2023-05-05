9608ed1d928629712eb9d22a04f8e8fe28cf4d28
Evandro Ribeiro <ribeiroevandro@live.com>
2023-05-05 09:36:39 -0300

    chore: update icons

--- better-vim/configs/devicons.lua
+++ better-vim/configs/devicons.lua
@@ -9,4 +9,14 @@ require("nvim-web-devicons").set_icon {
     color = "#428850",
     name = "Zsh"
   },
+  [".eslintrc.json"] = {
+    icon = " ",
+    color = "#4b32c3",
+    name = "Eslint"
+  },
+  ["tailwind.config.js"] = {
+    icon = "󱏿 ",
+    color = "#3b82f6",
+    name = "TailwindCSS"
+  }
 }
